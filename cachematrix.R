c1469c69f61c5e9360f7bc23b08227de2b3ce2e5
CHE-WEI <CHE-WEI@users.noreply.github.com>
2014-11-12 14:33:33 +0800

    Do the homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is used to store functions 
+## related to the data and the result.
+    ## It define 4 sub-function including 
+       ## input data matrix, 
+       ## return inputted matrix, 
+       ## input the result which is calculated from cacheSolve , 
+       ## and return the inputted result .
 
-## Write a short comment describing this function
+## cacheSolve is used to store functions
+## related to calculate the results and check the cache of result
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+
+## Created 4 subfunction. They are used in the way of cache  
+makeCacheMatrix <- function(x1 = numeric()) {
+        m1 <- NULL
+		
+        set <- function(y) {             ## Alternative way to input data
+                x1 <<- y
+                m1 <<- NULL              ## New data in, reset m1
+        }
+		
+        get <- function() {
+			return(x1)
+		}
+		
+        setinverse <- function(inverse) { ## Input value for m1  
+			m1 <<- inverse
+		}
+		
+        getinverse <- function() {        ## Print out m1 
+			return(m1)
+		}
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Calculate & Return a matrix that is the inverse of 'x' 
+cacheSolve <- function(x2, ...) {
+        m2 <- x2$getinverse()        ## Retrive m1 from makeCacheMatrix 
+                                     ## (renamed as m2 here)
+        
+		if(!is.null(m2)) {       ## If m1 in makeCacheMatrix isn't null 
+                message("getting cached data")  ## Print out the value of m1 
+                return(m2)
+        }
+        
+		data <- x2$get()         ## Import data  
+		m2 <- solve(data, ...)   ## Do the calculation 
+		x2$setinverse(m2)        ## Pass the value to makeCacheMatrix 
+        
+		return(m2)               ## Print the Result
 }
